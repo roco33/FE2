52bc52630c13a68cf4722b3b41c2529eede6909a
roco33 <minhaogu@gmail.com>
2017-11-10 14:04:33 -0600

    project2

--- Project2/project2.py
+++ Project2/project2.py
@@ -12,21 +12,25 @@ test error through different jmax
 
 import numpy as np
 
-nmin = 40 # jmax min 
-nmax = 500 # jmax max
-jmaxtep = 20 # test jmax in jmaxtep steps
-imax = 1000
-S0 = 40 # stock price
-X = 40 # strike 
-sig = 0.2 # stock volatility
-r = 0.06 # risk-free
-div = 0 # dividend yield
-T = 0.5 # time horizon
-smin = 0 # lower boundary
-smax = 100 # upper boundary
+Input = {
+        'nmin' : 40, # jmax min 
+        'nmax' : 500, # jmax max
+        'jmaxtep' : 20, # test jmax in jmaxtep steps
+        'imax' : 1000, # imax
+        'S0' : 40, # stock price
+        'X' : 40, # strike 
+        'sig' : 0.2, # stock volatility
+        'r' : 0.06, # risk-free
+        'div' : 0, # dividend yield
+        'T' : 0.5, # time horizon
+        'smin' : 0, # lower boundary
+        'smax' : 100, # upper boundary
+        }
+
 
 delt = T / imax # dt
 dels = (smax - smin) / jmax # ds
+
 
 
 
@@ -46,12 +50,40 @@ def payoff(jmax, dels, delt, smin, sig, r, div,):
 
 
 def bounaryCond():
-    for i in range(imax-1,-1,-1):
-        pass
-    return
+    for i in range(1,imax+1,1):
+        # lower boundary
+        A[0] = 0
+        B[0] = 1
+        C[0] = 0
+        D[0] = 0
+        # D accross j levels
+        for j in range(1,jmax,1):
+            D1 = -V[j - 1] * (0.25 * sig ** 2 * j ** 2 - 0.25 * (r - div) * j)
+            D2 = -V[j] * (1 / delt - 0.5 * r - 0.5 * sig ** 2 * j ^ 2)
+            D3 = -V[j + 1] * (0.25 * sig ** 2 * j ** 2 + 0.25 * (r - div) * j)
+            D[j] = D1 + D2 + D3
+        # upper boundary equations
+        A[jmax] = 0
+        B[jmax] = 1
+        C[jmax] = 0
+        D[jmax] = smax * np.exp(-div * (i * delt)) - X * np.exp(-r * (1 * delt))
+    return A, B, C, D
 
 
-def main():
+def LU():
+    alpha[0] = B[0]
+    Sum[0] = D[0]
+    for j in range(1, jmax + 1, 1):
+        alpha[j] = B[j] - ((A[j] * C[j - 1]) / alpha[j - 1])
+        Sum[j] = D[j] - ((A[j] * Sum[j - 1]) / alpha[j - 1])
+    V[jmax] = Sum[jmax] / alpha[jmax]
+    for j in range(jmax-1, -1, -1):
+        V[j] = (Sum[j] - C[j] * V[j + 1]) / alpha[j]
+    return V
+        
+        
+def main(I):
+    
     #payoff
     #boundary condition
     #   LU decomposition
