73225173610694c28af60c3abe185dc43574b573
roco33 <minhaogu@gmail.com>
2017-11-13 22:59:57 -0600

    project2

--- Project2/project2.py
+++ Project2/project2.py
@@ -7,18 +7,21 @@ Created on Sat Nov 11 03:05:48 2017
 
 import numpy as np
 
-jmax = 100
 imax = 1250
 s0 = 156.99
-D = 15.875
-T = 1.25
-r = 0.02
+smin = 0
+smax = 3 * s0
+TAC = 100 #auto call index, should be multiplier of 10
+dels = s0 / TAC # delta s
+TR = 0.7 * s0 / dels # trigger index
+jmax = 3 * TAC 
+cou = 15.875 # coupon
+T = 1.25 # maturity
+r = 0.02 
 sig = 0.2
 div = 0.02
-smin = 0
-smax = 330
+
 delt = T / imax
-dels = (smax - smin) / jmax
 
 
 s = np.zeros(jmax + 1)
@@ -26,91 +29,8 @@ A = np.zeros(jmax + 1)
 B = np.zeros(jmax + 1)
 C = np.zeros(jmax + 1)
 D = np.zeros(jmax + 1)
-V = np.zeros(jmax + 1)
-
-
-# triggered
-VT = np.zeros(jmax + 1)
-for j in range(jmax + 1):
-    s[j] = smin + j * dels
-    
-    if s[j] / s0 < 0.7:
-        VT[j] = s[j] / s0 * 1000
-    elif (s[j] / s0 >= 0.7) & (s[j] / s0 < 1):
-        VT[j] = s[j] / s0 * 1000 + D
-    else:
-        VT[j] = 1000 + D
-    # A, B, C [0 to jmax]
-    A[j] = 0.25 * sig ** 2 * j ** 2 - 0.25 * (r - div) * j
-    B[j] = -1 / delt - 0.5 * r - 0.5 * sig ** 2 * j ** 2
-    C[j] = 0.25 * sig ** 2 * j ** 2 + 0.25 * (r - div) * j
-
-for i in range(imax - 1, -1, -1):
-    # lower boundary
-    A[0] = 0
-    B[0] = 1
-    C[0] = 0
-    D[0] = B[0] * V[0] + C[0] * VT[1] 
-    # D across 1 to (jmax-1)
-    for j in range(1, jmax, 1):
-        if i % (imax / 5) == 0:
-            if s[j] >= s0:
-                VT[j] = 1000
-            elif s[j] / s0 >= 0.7 and s[j] / s0 < 1:
-                VT[j] = VT[j] + D
-        D1 = -VT[j - 1] * A[j]
-        D2 = -VT[j] * B[j]
-        D3 = -VT[j + 1] * C[j]
-        D[j] = D1 + D2 + D3
-    A[jmax] = 0
-    B[jmax] = 1
-    C[jmax] = 0
-    n = int(i / (imax / 5)) * (imax / 5) # the last review period 
-    D[jmax] = (1000 + D) * np.exp(-div * ((i - n) * delt))
-    
-    VT = LU(jmax, A, B, C, D, VT)
-
-# not triggered
-VNT = np.zeros(jmax + 1)
-for j in range(70, jmax + 1, 1):
-    s[j] = smin + j * dels 
-    
-    if s[j] / s0 < 0.7:
-        VNT[j] = 1000
-    else:
-        VNT[j] = 1000 + D
-    A[j] = 0.25 * sig ** 2 * j ** 2 - 0.25 * (r - div) * j
-    B[j] = -1 / delt - 0.5 * r - 0.5 * sig ** 2 * j ** 2
-    C[j] = 0.25 * sig ** 2 * j ** 2 + 0.25 * (r - div) * j
-
-for i in range(jmax + 1, -1, -1):
-    # lower boundary
-    A[0] = 0
-    B[0] = 1
-    C[0] = 0
-    D[0] = B[0] * V[0] + C[0] * VNT[1]
-    # D accross 1 to (jmax+1)
-    for j in range(1, jmax, 1):
-        if i % (imax / 5) == 0:
-            if s[j] >= s0:
-                VNT[j] = 1000
-            elif s[j] / s0 >= 0.7 and s[j] / s0 < 1:
-                VNT[j] = VNT[j] + D
-        D1 = VNT[j - 1] * A[j]
-        D2 = VNT[j] * B[j]
-        D3 = VNT[j + 1] * C[j]
-        D[j] = D1 + D2 + D3
-    A[jmax] = 0
-    B[jmax] = 1
-    C[jmax] = 0
-    n = int(i / (imax / 5)) * (imax / 5) # the last review period 
-    D[jmax] = (1000 + D) * np.exp(-div * ((i - n) * delt))
-    
-    VNT = LU(jmax, A, B, C, D, VNT)
-
-
-
-
+V = np.zeros((imax + 1, jmax + 1))
+VT = np.zeros((imax + 1, jmax + 1))
 
 
 def LU(jmax, A, B, C, D, V):
@@ -125,3 +45,93 @@ def LU(jmax, A, B, C, D, V):
     for j in range(jmax - 1, -1, -1):
         V[j] = 1 / alpha[j] * (S[j] - C[j] * V[j + 1])
     return V
+
+
+# triggered
+
+# at maturity
+for j in range(jmax + 1):
+    s[j] = smin + j * dels
+    
+    if s[j] / s0 < 0.7:
+        VT[imax+1,j] = s[j] / s0 * 1000
+    elif (s[j] / s0 >= 0.7) and (s[j] / s0 < 1):
+        VT[imax+1,j] = s[j] / s0 * 1000 + cou
+    else:
+        VT[imax+1,j] = 1000 + cou
+    # A, B, C [0 to jmax]
+    A[j] = 0.25 * sig ** 2 * j ** 2 - 0.25 * (r - div) * j
+    B[j] = -1 / delt - 0.5 * r - 0.5 * sig ** 2 * j ** 2
+    C[j] = 0.25 * sig ** 2 * j ** 2 + 0.25 * (r - div) * j
+
+for i in range(imax - 1, -1, -1):
+    # lower boundary
+    A[0] = 0
+    B[0] = 1
+    C[0] = 0
+    D[0] = B[0] * V[0] + C[0] * VT[1] 
+    # D across 1 to (jmax-1)
+    for j in range(1, jmax, 1):
+        D1 = -VT[i+1,j - 1] * A[j]
+        D2 = -VT[i+1,j] * B[j]
+        D3 = -VT[i+1,j + 1] * C[j]
+        D[j] = D1 + D2 + D3
+        if i % (imax / 5) != 0: # not review date
+            A[jmax] = 0    
+            B[jmax] = 1
+            C[jmax] = 0
+            n = int(i / (imax / 5)) * (imax / 5) # the last review period 
+            D[jmax] = (1000 + cou) * np.exp(-(r - div) * ((i - n) * delt)) 
+            VT[i,:] = LU(jmax, A, B, C, D, VT)
+        elif i % (imax / 5) == 0: # review date
+            VT[TAC] = 1000
+            A[TAC] = 0
+            B[TAC] = 1
+            C[TAC] = 0
+            D[TAC] = 1000
+            VT[0: TAC + 1] = LU(TAC, A[0: TAC + 1], B[0: TAC + 1], C[0: TAC + 1], D[0: TAC + 1], VT[0: TAC + 1])
+            for j in range(TAC + 1, jmax + 1, 1):
+                VT[j] = 1000          
+                    
+
+# not triggered
+V = np.zeros(jmax + 1)
+# TR to jmax
+for j in range(TR, jmax + 1, 1):
+    s[j] = smin + j * dels 
+    
+    if s[j] / s0 < 0.7:
+        V[j] = 1000
+    else:
+        V[j] = 1000 + cou
+    
+for i in range(imax - 1, -1, -1):
+    # lower boundary
+    A[0] = 0
+    B[0] = 1
+    C[0] = 0
+    D[0] = B[0] * V[0] + C[0] * V[1]
+    # D accross 1 to (jmax+1)
+    for j in range(1, jmax, 1):
+        if i % (imax / 5) == 0:
+            if s[j] >= s0:
+                V[j] = 1000
+            elif s[j] / s0 >= 0.7 and s[j] / s0 < 1:
+                V[j] = V[j] + cou
+        D1 = V[j - 1] * A[j]
+        D2 = V[j] * B[j]
+        D3 = V[j + 1] * C[j]
+        D[j] = D1 + D2 + D3
+    A[jmax] = 0
+    B[jmax] = 1
+    C[jmax] = 0
+    n = int(i / (imax / 5)) * (imax / 5) # the last review period 
+    D[jmax] = (1000 + cou) * np.exp(-(r-div) * ((i - n) * delt))
+    
+    VNT = LU(jmax, A, B, C, D, V)
+
+
+
+
+
+
